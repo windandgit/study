223556b8d90623032a0e404337cfb6582b0bf90d
bbb <bbb@qq.com>
2021-07-06 11:31:13 +0800

    新创建分支branch-two，第三次提交

--- src/main/java/com/test/gitTest/GItTest.java
+++ src/main/java/com/test/gitTest/GItTest.java
@@ -5,5 +5,7 @@ public class GItTest {
         System.out.println("我是用来测试git的联合开发的，我是用户bbb");
         System.out.println("------------------------------------------");
         System.out.println("bbb二次提交，这次测试的是分支");
+        System.out.println("------------------------------------------");
+        System.out.println("分支two提交---新创建的分支branch-two，第三次提交");
     }
 }
