082bf4d2bbd813c8b7716c2bbe64a2c5e0d501c2
aaa <aaa@qq.com>
2021-07-06 14:13:53 +0800

    测试分支合并master，先从分支3提交到远程分支3

--- src/main/java/com/test/gitTest/GItTest.java
+++ src/main/java/com/test/gitTest/GItTest.java
@@ -5,5 +5,6 @@ public class GItTest {
         System.out.println("我是用来测试git的联合开发的，我是用户bbb");
         System.out.println("------------------------------------------");
         System.out.println("bbb二次提交，这次测试的是分支");
+        System.out.println("测试分支合并master，我们先提交到master");
     }
 }
